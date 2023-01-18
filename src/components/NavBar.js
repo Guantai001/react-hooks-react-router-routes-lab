66f5fa3f5a4a23d8b563b720f70b829922b1ec6e
guantai <jpguantai@gmail.com>
2023-01-18 09:15:08 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,54 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+function NavBar({ link}) {
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        // add styling to Navlink
+        style={link}
+        // add prop for activeStyle
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        style={link}
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        style={link}
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={link}
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Actors
+      </NavLink>
+    </div>
+  );
+
 }
 
 export default NavBar;
